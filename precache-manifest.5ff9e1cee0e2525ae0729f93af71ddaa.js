3e9a69cf5429a190839e9c83ba82b7c5a11f733a
paulloclara <paulloclara@gmail.com>
2020-07-22 11:30:15 -0300

    4e46cd9

---
+++ precache-manifest.5ff9e1cee0e2525ae0729f93af71ddaa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed88de19827d382c9f7b",
+    "url": "/currency-converter/css/app.3b299020.css"
+  },
+  {
+    "revision": "828ac01feb3476dcb597bbc390558b60",
+    "url": "/currency-converter/img/api.828ac01f.svg"
+  },
+  {
+    "revision": "efdc77104cab003c5978e8b792cc35d1",
+    "url": "/currency-converter/img/down.efdc7710.svg"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/currency-converter/img/github.5cff78e4.svg"
+  },
+  {
+    "revision": "46ff14f0b607261568fc630742e45c97",
+    "url": "/currency-converter/img/preview.png"
+  },
+  {
+    "revision": "4e2c0a1d4a1adb1a99a69310e06fbbc7",
+    "url": "/currency-converter/img/up.4e2c0a1d.svg"
+  },
+  {
+    "revision": "ec2ea5c1a9894b6bef25afd1c555e627",
+    "url": "/currency-converter/img/variation.ec2ea5c1.svg"
+  },
+  {
+    "revision": "7a431d9f8e3ac0b001bc18310e91a1b0",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "ed88de19827d382c9f7b",
+    "url": "/currency-converter/js/app.9d72ab31.js"
+  },
+  {
+    "revision": "e29628db88c4cee3b6cc",
+    "url": "/currency-converter/js/chunk-vendors.e14b65d8.js"
+  },
+  {
+    "revision": "b07feb03095e0615a461a066f9dd8b59",
+    "url": "/currency-converter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/currency-converter/robots.txt"
+  }
+]);
